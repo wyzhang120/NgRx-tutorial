d880142d1d1e42ad46f66b92e2237139b991f8f1
Wenyuan Zhang <wzhang45@slb.com>
2022-08-10 11:34:13 +0800

    updated ngrx tutorial

---
+++ BookCollection/src/app/state/books.actions.ts
@@ -0,0 +1,23 @@
+import { createAction, props } from '@ngrx/store';
+import { Book } from '../book-list/books.model';
+
+export const addBook = createAction(
+  '[Book List] Add Book',
+  props<{ bookId: string }>()
+);
+
+export const removeBook = createAction(
+  '[Book Collection] Remove Book',
+  props<{ bookId: string }>()
+);
+
+export const retrievedBookList = createAction(
+  '[Book List/API] Retrieve Books Success',
+  props<{ books: ReadonlyArray<Book> }>()
+);
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
